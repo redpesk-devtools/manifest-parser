d3ff6dabb4bad29621286d94b91eedf04db7bbb8
Louis-Baptiste Sobolewski <lb.sobolewski@iot.bzh>
2024-11-15 16:57:45 +0100

    Use exc_info instead of logger.exception
    Signed-off-by: Louis-Baptiste Sobolewski <lb.sobolewski@iot.bzh>

--- src/common.py
+++ src/common.py
@@ -15,18 +15,19 @@ def load_manifest(path: str, logger: logging.Logger) -> dict | None:
         with open(path, mode="r", encoding="utf-8") as manifest_file:
             manifest = yaml.safe_load(manifest_file)
     except OSError:
-        logger.exception("%s could not be read", path)
-        logger.critical("The file could not be read (check path and permission)")
+        logger.critical(
+            "%s could not be read (check path and permission)", path, exc_info=True
+        )
         return None
     except yaml.parser.ParserError:
-        logger.exception("%s does not look like a valid YAML file", path)
-        logger.critical("The file could not be parsed")
+        logger.critical("%s does not look like a valid YAML file", path, exc_info=True)
         return None
     except yaml.scanner.ScannerError:
-        logger.exception("%s does not look like a valid YAML file", path)
         logger.critical(
-            "The file could not be parsed"
-            " (check that you don't use @ or ` which are reserved YAML characters)"
+            "%s does not look like a valid YAML file"
+            " (check that you don't use @ or ` which are reserved YAML characters)",
+            path,
+            exc_info=True,
         )
         logger.warning(
             "Know that this program only works on complete YAML files"
